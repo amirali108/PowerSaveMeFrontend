2ea397bd8826d0a24a0bde4244a755ccc45a667b
Amirali Fatehi <117827939+amirali108@users.noreply.github.com>
2024-09-02 14:54:05 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-import './app.css'
+import './App.css'  // Corrected to match the actual file name
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
